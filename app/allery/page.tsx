a28e2b858dc3682b8fc46a2ec6338ece1efc766d
streminiai <streminiai@gmail.com>
2025-11-01 23:21:56 +0530

    Create page.tsx

---
+++ app/allery/page.tsx
@@ -0,0 +1,11 @@
+import { Gallery } from "@/components/sections/gallery"
+
+export default function GalleryPage() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <section className="py-16 md:py-24">
+        <Gallery />
+      </section>
+    </main>
+  )
+}
